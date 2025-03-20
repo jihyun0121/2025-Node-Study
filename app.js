6001edf54f53b693e54b8003748158c572d14394
gheun <gheun@DESKTOP-ARR6LC5>
2025-03-21 08:57:34 +0900

    post 요청

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello, World!\n');
 });
 
+app.get('/swag', (req, res) => {
+  res.send('get swag');
+});
+
+app.post('/swag', (req, res) => {
+  res.send('post swag');
+});
+
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
