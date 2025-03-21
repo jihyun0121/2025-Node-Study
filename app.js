b6177e7a39272b0d80843d5959688895c189d05b
gheun <gheun@DESKTOP-ARR6LC5>
2025-03-21 09:59:21 +0900

    router로 파일을 별도 관리

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const express = require('express');
+const swagRouter = require('./routes/swag');
+
 const app = express();
-const port = 3000;
 
 app.use(express.json());
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, World!\n');
-// });
-
-
-app.get('/swag', (req, res) => {
-  res.send('get swag');
-});
-
-// app.post('/swag', (req, res) => {
-//   res.send(req.body);
-// });
-
-app.get('/swag/:person', (req, res) => {
-  res.send(req.params.person);
-});
+app.use('/swag', swagRouter);
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
