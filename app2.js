14bc017c95fc5e3da236a09a380982eb431ac3b7
gheun <gheun@DESKTOP-ARR6LC5>
2025-03-28 10:00:20 +0900

    MySQL 연결 & dotenv

--- app2.js
+++ app2.js
@@ -1,14 +1,35 @@
+const { error } = require('console');
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 
-const travelList = ['뉴욕', '파리', '서울', '도쿄'];
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+travelList = ['뉴욕', '파리', '서울', '도쿄'];
 
 app.set('view engine', 'ejs');
 // _dirname : 현재 파일이 속한 절대경로
 // path.join()을 사용하면 운영체제에 맞추어 경로 구분자(/, \)를 알아서 정해준다
 app.set('views', path.join(__dirname, 'views'));
+
+db.connect(error => {
+    if (error) {
+        console.log('MySQL 연결 실패: ', error);
+        return;
+    }
+    console.log('MySQL에 연결 되었습니다.');
+});
+
+
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -19,5 +40,5 @@ app.get('/travel', (req, res) => {
 });
 
 app.listen(3001, () => {
-    console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+    console.log(`서버가 http://localhost:3001 에서 실행 중입니다.`);
   });
